c2647f031f19c48adff7f45fd567a1e033cf9e9f
Nicoleta Tîrsîna <151368781+Nika0304@users.noreply.github.com>
2026-04-05 01:52:08 +0300

    Add parser test file

---
+++ tests/testparser.c
@@ -0,0 +1,34 @@
+struct Pt{
+    int x;
+    int y;
+};
+int a;
+double v[10];
+struct Pt p;
+
+// struct Pt{
+// 	int x;
+// 	int y;
+// 	};
+// struct Pt points[10];
+
+// double max(double a,double b){
+// 	if(a>b)return a;
+// 		else return b;
+// 	}
+
+// int len(char s[]){
+// 	int i;
+// 	i=0;
+// 	while(s[i])i=i+1;
+// 	return i;
+// 	}
+
+// void main(){
+// 	int i;
+// 	i=10;
+// 	while(i!=0){
+// 		puti(i);
+// 		i=i/2;
+// 		}
+// 	}
